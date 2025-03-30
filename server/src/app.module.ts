d81b332d4bc5b3cda25389aad2083737d69cb5b5
olithom <olithom@gmail.com>
2025-03-30 16:51:04 +0200

    NestJs server static

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CronService } from './cron/cron.service';
 import { ArduinoService } from './arduino/arduino.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
-    ScheduleModule.forRoot()
+    ScheduleModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'web-app/browser'),
+      // serveRoot: '/static/',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, CronService, ArduinoService],
